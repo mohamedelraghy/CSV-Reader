38fefbd02d4ed47cc1085ef920d79027da6a6ba3
Mohamed EL-Raghy <elraghy8@gmail.com>
2023-10-28 17:35:16 +0200

    use composition

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResultEnum";
 
-const csvFileReader = new MatchReader("football.csv");
+// * create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader("football.csv");
 
-csvFileReader.read();
-const matches = csvFileReader.data;
+// * create an instance of MatchReader & pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWin = 0;
 
-for (const match of matches) {
+for (const match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin)
     manUnitedWin++;
   else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
