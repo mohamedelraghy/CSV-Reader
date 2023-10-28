9cbfb76692a1987b4e8f0683b907a95566d3101d
Mohamed EL-Raghy <elraghy8@gmail.com>
2023-10-28 14:19:48 +0200

    transforming data

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,6 @@
 import { readFileSync } from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResultEnum";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public filename: string) {}
 
@@ -14,17 +9,6 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
       });
   }
 }
